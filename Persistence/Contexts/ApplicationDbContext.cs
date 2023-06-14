78d9ae0a0c3bb5068012e349c81398be740174ea
yonaaani <yonaaani@gmail.com>
2023-06-14 21:48:24 +0300

    додала ще раз Persistence, не знаю в чому була помилка, але не відкривався цей проект, вже все нормально :)

---
+++ Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Orders> Orders => Set<Orders>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+
+    }
+}
---
+++ Persistence/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+using Persistence.Repositories;
+
+namespace Persistence.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext(configuration);
+            services.AddRepositories();
+        }
+
+        private static void AddDbContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString(@"Data Source=DESKTOP-AOKJEDU\SQLEXPRESS;Database=Project_BookStore;Integrated Security=true;");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+               options.UseSqlServer(connectionString));
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services
+                .AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork))
+                .AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        }
+    }
+}
